0529612238fd8db459eae13e075c8394319a73f8
OjusAnilNaik <ojusnaik26@gmail.com>
2026-08-19 10:46:21 +0530

    Issue fixed

--- efficientPrime.java
+++ efficientPrime.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class effPrime {
+public class efficientPrime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
