f587efecf75ae133030b17556f949d4438911205
OjusAnilNaik <ojusnaik26@gmail.com>
2026-08-29 10:00:26 +0530

    Java Programs

--- return.java
+++
@@ -1,8 +0,0 @@
-class check {
-    public static void main(String[] args) {
-        int num = 29;
-        if (num > 10) {
-            System.out.println("Number is greater than 10");
-        } 
-    }
-}
